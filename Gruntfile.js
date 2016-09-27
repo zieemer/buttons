976bee731b466a2f24301711a85902dd8b0389d7
sylwester_z <Sylwester.Ziemer@gap.uk.com>
2016-09-27 09:41:54 +0100

    bower addded

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+﻿/*global module */
+module.exports = function (grunt) {
+    'use strict';
+
+
+   
+    grunt.initConfig({
+        // read in the project settings from the package.json file into the pkg property
+        pkg: grunt.file.readJSON('package.json'),
+
+        // define configuration for each of the tasks we have
+        // this is a sample jshint task config
+        /*
+            jshint: {
+                    // define the files to lint
+                    files: ['gruntfile.js', 'src/*.js', '/*.js'],
+                    // configure JSHint (documented at http://www.jshint.com/docs/)
+                    options: {
+                    // more options here if you want to override JSHint defaults
+                    globals: {
+                            jQuery: true,
+                            console: true,
+                            module: true
+                            }
+                        }
+                    }
+        */
+
+        bowerInstall: {
+ 
+        target: {
+ 
+            // Point to the files that should be updated when 
+            // you run `grunt bower-install` 
+                src: [
+                  'index.html',   // .html support... 
+                  'app/views/**/*.jade',   // .jade support... 
+                  'app/styles/main.scss',  // .scss & .sass support... 
+                  'app/config.yml'// and .yml & .yaml support out of the box! 
+                ],
+ 
+            // Optional: 
+            // --------- 
+                cwd: '',
+                dependencies: true,
+                devDependencies: false,
+                exclude: [],
+                fileTypes: {},
+                ignorePath: '',
+                overrides: {}
+        }
+    }
+    });
+
+    // Add all plugins that your project needs here
+    grunt.loadNpmTasks('grunt-bower-install');
+
+    // this would be run by typing "grunt test" on the command line
+    // the array should contains the names of the tasks to run
+    grunt.registerTask('test', []);
+
+    // define the default task that can be run just by typing "grunt" on the command line
+    // the array should contains the names of the tasks to run
+    grunt.registerTask('default', []);
+};
